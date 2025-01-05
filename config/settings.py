7c26a32acd6201b98f6c9cf1947d0b379a296568
David Akinola <dakinola54@gmail.com>
2025-01-05 17:28:26 +0100

    Login functionality

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = 'home'
