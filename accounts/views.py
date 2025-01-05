7f1cf32f06d2ee733c355b3d3c7e67cd6b591936
David Akinola <dakinola54@gmail.com>
2025-01-05 17:45:24 +0100

    signup functionality

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
